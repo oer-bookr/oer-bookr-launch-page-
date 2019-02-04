73e9be4abcaf63c6c303814400e8f7ed8458e38f
Jacob Angulo <jacobangulo@hotmail.com>
2019-02-04 13:54:01 -0800

    felt like a good place to commit, in process of creating a class that will make contnent sections via DOM stuff

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,4 @@
+// scrolly funcs
 const downBtn = document.querySelectorAll('.down-button').forEach(link => {
     link.addEventListener('click', event => {
         let elmntHeight = document.querySelector('.content-box').clientHeight;
@@ -11,3 +12,39 @@ const upBtn = document.querySelectorAll('.up-button').forEach(link => {
         window.scrollBy(0, -elmntHeight)
     })
 })
+
+// something I gotta describe l8r
+const contentArray = [];
+const mainContent = document.querySelector('.main-content');
+
+// content box contructor
+class ContentBox {
+    constructor(content) {
+        this.content = content;
+        contentArray.push(this.content);
+        this.section = document.createElement('section');
+        this.section.classList.add('content-box');
+        this.upScrolly = document.createElement('a');
+        this.upScrolly.classList.add('up-button');
+        
+        this.downScrolly = document.createElement('a');
+        this.downScrolly.classList.add('up-button');
+    }
+
+    BoxLayerOuter() {
+        console.log(contentArray)
+    }
+}
+
+let contentBox1 = new ContentBox ({
+    "content box": 1,
+    "textContnent": "I am the text content for content box 1"
+})
+
+let contentBox2 = new ContentBox ({
+    "content box": 2,
+    "textContnent": "I am the text content for content box 2"
+})
+
+console.log(contentArray.length);
+
