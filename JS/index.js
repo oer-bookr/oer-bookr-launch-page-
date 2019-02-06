52429edb2a185b4c68e6ea002812192e4c392a1a
Jacob Angulo <jacobangulo@hotmail.com>
2019-02-05 20:18:37 -0800

    random quote generator

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,5 @@
 // for use in random quote generator
-// Math.round(Math.random() * 10)
+// Math.ceil(Math.random() * 10)
 
 // content box contructor class
 class ContentBox {
@@ -59,6 +59,30 @@ let contentArray = [
 ];
 
 contentArray.forEach(content => new ContentBox(content));
+
+
+// random quote generator
+let quotes = [
+    {"index": 1,"author": "-Thomas Jefferson", "quote": "\"The glow of one warm thought is to me worth more than money.\""},
+    {"index": 2,"author": "-E. E. Cummings", "quote": "\"Once we believe in ourselves, we can risk curiosity, wonder, spontaneous delight, or any experience that reveals the human spirit.\""},
+    {"index": 3,"author": "-Joyce Meyer", "quote": "\"A No. 2 pencil and a dream can take you anywhere.\""},
+    {"index": 4,"author": "-Wilma Rudolph", "quote": "\"When the sun is shining I can do anything; no mountain is too high, no trouble too difficult to overcome.\""},
+    {"index": 5,"author": "-Nathaniel Hawthorne", "quote": "\"Happiness is a butterfly, which when pursued, is always just beyond your grasp, but which, if you will sit down quietly, may alight upon you.\""},
+    {"index": 6,"author": "-Andrew Hendrixson", "quote": "\"Anyone who has ever made anything of importance was disciplined.\""},
+    {"index": 7,"author": "-Henry David Thoreau", "quote": "\"What you get by achieving your goals is not as important as what you become by achieving your goals.\""},
+    {"index": 8,"author": "-Author Unknown", "quote": "\"If the plan doesn’t work, change the plan, but never the goal.\""},
+    {"index": 9,"author": "-Neil Barringham", "quote": "\"The grass is greener where you water it.\""},
+    {"index": 10,"author": "-Yoda", "quote": "\"Do or do not. There is no try.\""}
+];
+
+window.addEventListener('load', event => {
+    let randomIndex = (Math.ceil(Math.random() * 10));
+    let randomQuote = quotes.filter(quote => quote["index"] == randomIndex);
+    document.querySelector('.quote').textContent = randomQuote[0]["quote"];
+    document.querySelector('.author').textContent = randomQuote[0]["author"];
+    console.log(randomQuote[0]["quote"]);
+    console.log(randomQuote[0]["author"]);
+})
 
 
 // scrolly func
